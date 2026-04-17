62beec4e88fd24b2429a61163e925e2cfb06d98a
jayamproj-dotcom <jayamproj@gmail.com>
2026-04-17 17:29:52 +0530

    build Change

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/vidyasthanam',
   assetPrefix: '/vidyasthanam/',
+  output: 'standalone',
   images: {
     qualities: [25, 50, 75, 85, 100],
   },
